9402e927025a839d0a24dfe2efbf83f9e087b810
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-06-29 08:18:21 +0200

    feat: count chaacters

--- stats.py
+++ stats.py
@@ -1,15 +1,8 @@
+from collections import Counter
+
 def book_text_number (book_text):
     num_words = book_text.split()
     return len(num_words)
 
 def book_character_occurences (book_text):
-    characters = list(book_text)
-    char_dict = {}
-    for char in characters:
-        lower_char = char.lower()
-        if lower_char in char_dict:
-            char_dict[lower_char] += 1
-        else:
-            char_dict[lower_char] = 1
-
-    return char_dict
+    return Counter(char.lower() for char in book_text)
