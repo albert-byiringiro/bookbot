a7dcf79f71a5df79bb9798d12cab98082959483a
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-06-29 09:32:11 +0200

    feat: add frequency sorting and reporting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import book_text_number, book_character_occurences
+from stats import frequency_sort, count_words, character_frequencies
 
 def get_book_text (path_to_file):
     with open(path_to_file, 'r') as file:
@@ -6,10 +6,22 @@ def get_book_text (path_to_file):
     return file_contents
 
 def main ():
+    print("============ BOOKBOT ============")
+
     book_text = get_book_text('./books/frankenstein.txt')
-    num_words = book_text_number(book_text)
-    # print(f"{num_words} words found in the document")
-    print(book_character_occurences(book_text))
+    print("Analyzing book found at books/frankenstein.txt...")
+    
+    print("----------- Word Count ----------")
+    num_words = count_words(book_text)
+    print(f"Found {num_words} total words")
+
+    print("--------- Character Count -------")
+    book_occurences = character_frequencies(book_text)
+    sorted_counts = frequency_sort(book_occurences)
+    for char, count in sorted_counts:
+        print(f"{char}: {count}")
+    
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,8 +1,23 @@
 from collections import Counter
 
-def book_text_number (book_text):
+def count_words (book_text):
     num_words = book_text.split()
     return len(num_words)
 
-def book_character_occurences (book_text):
-    return Counter(char.lower() for char in book_text)
+def character_frequencies (book_text):
+    characters = list(book_text)
+    char_dict = {}
+    for char in characters:
+        lower_char = char.lower()
+        if lower_char in char_dict:
+            char_dict[lower_char] += 1
+        else:
+            char_dict[lower_char] = 1
+
+    return char_dict
+
+def frequency_sort (book_occurences):
+    list_occurences = book_occurences.items()
+    sorted_by_frequency = sorted(list_occurences, key=lambda x: x[1], reverse=True)
+    lists = [occ for occ in sorted_by_frequency if occ[0].isalpha()]
+    return lists
