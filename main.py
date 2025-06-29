7632f047de614bd70e3f47de7cda243ada7d4f6b
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-06-29 07:12:42 +0200

    feat: add boot text number display

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ def get_book_text (path_to_file):
         file_contents = file.read()
     return file_contents
 
+def book_text_number (book_text):
+    num_words = book_text.split()
+    return len(num_words)
 
 def main ():
-    print(get_book_text('./books/frankenstein.txt'))
+    book_text = get_book_text('./books/frankenstein.txt')
+    num_words = book_text_number(book_text)
+    print(f"{num_words} words found in the document")
 
 
 main()
