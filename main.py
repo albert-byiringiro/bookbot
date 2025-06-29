5c894eafce7a1b841439b3cdbdd360da64c9e249
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-06-29 07:16:49 +0200

    feat: separate code into separate files

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import book_text_number
+
 def get_book_text (path_to_file):
     with open(path_to_file, 'r') as file:
         file_contents = file.read()
     return file_contents
-
-def book_text_number (book_text):
-    num_words = book_text.split()
-    return len(num_words)
 
 def main ():
     book_text = get_book_text('./books/frankenstein.txt')
---
+++ stats.py
@@ -0,0 +1,3 @@
+def book_text_number (book_text):
+    num_words = book_text.split()
+    return len(num_words)
