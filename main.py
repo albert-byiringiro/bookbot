49dda129c29bc40b6f6ccd547ce5f075070ae8d4
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-06-29 10:01:24 +0200

    feat: make functions dynamic

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
+import sys
 from stats import frequency_sort, count_words, character_frequencies
 
 def get_book_text (path_to_file):
-    with open(path_to_file, 'r') as file:
+    with open(path_to_file, 'r', encoding='utf-8') as file:
         file_contents = file.read()
     return file_contents
 
-def main ():
+def main():
     print("============ BOOKBOT ============")
 
-    book_text = get_book_text('./books/frankenstein.txt')
-    print("Analyzing book found at books/frankenstein.txt...")
-    
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    try:
+        book_text = get_book_text(sys.argv[1])
+    except FileNotFoundError:
+        print(f"Error: File '{sys.argv[1]}' not found.")
+        sys.exit(1)
+
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     num_words = count_words(book_text)
     print(f"Found {num_words} total words")
-
     print("--------- Character Count -------")
     book_occurences = character_frequencies(book_text)
     sorted_counts = frequency_sort(book_occurences)
     for char, count in sorted_counts:
         print(f"{char}: {count}")
-    
     print("============= END ===============")
-
 
 main()
