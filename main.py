6d2458239a4c5e0ae1500a4dfb7535704b6d7920
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-06-29 07:00:56 +0200

    feat: add function to read file

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text (path_to_file):
+    with open(path_to_file, 'r') as file:
+        file_contents = file.read()
+    return file_contents
+
+
+def main ():
+    print(get_book_text('./books/frankenstein.txt'))
+
+
+main()
