4e499851a001254094351c20a7b1ec6e46c5d4a9
Albert Byiringiro <byiringiroalbert20@gmail.com>
2025-06-29 07:49:37 +0200

    feat: add book character occurences counter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import book_text_number
+from stats import book_text_number, book_character_occurences
 
 def get_book_text (path_to_file):
     with open(path_to_file, 'r') as file:
@@ -8,7 +8,8 @@ def get_book_text (path_to_file):
 def main ():
     book_text = get_book_text('./books/frankenstein.txt')
     num_words = book_text_number(book_text)
-    print(f"{num_words} words found in the document")
+    # print(f"{num_words} words found in the document")
+    print(book_character_occurences(book_text))
 
 
 main()
